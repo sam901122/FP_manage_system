8263a74a38053ce4b12b0494b1d3b8446e193fb3
PuSung <sam90.hsu@gmail.com>
2024-03-06 19:03:14 +0800

    tidy up main.go

--- main.go
+++ main.go
@@ -1,17 +1,3 @@
-// Copyright 2016 LINE Corporation
-//
-// LINE Corporation licenses this file to you under the Apache License,
-// version 2.0 (the "License"); you may not use this file except in compliance
-// with the License. You may obtain a copy of the License at:
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations
-// under the License.
-
 package main
 
 import (
@@ -27,14 +13,13 @@ import (
 )
 
 func main() {
+	// Setup message API client
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
 	channelSecret := os.Getenv("LINE_CHANNEL_SECRET")
 
-	fmt.Print("channelSecret: ", channelSecret, "\n")
 	bot, err := messaging_api.NewMessagingApiAPI(
 		os.Getenv("LINE_CHANNEL_TOKEN"),
 	)
